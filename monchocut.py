9ad056811a1b34f7b008cd6cefef41d3cc2003f5
Your Name <you@example.com>
2026-09-15 01:25:29 +0000

    propuesta de emprolijada por copilot

--- monchocut.py
+++ monchocut.py
@@ -1,126 +1,95 @@
 import csv
-import yaml
+from random import choice
+
 import matplotlib.pyplot as plt
+import yaml
 from matplotlib.patches import Rectangle
+from PIL.ImageColor import colormap
 from xlsxwriter import Workbook
 
-from rectpack import newPacker, PackingBin
-# Possible optimizing algorithims:
-# https://github.com/secnot/rectpack/blob/master/rectpack/maxrects.py
-#  - MaxRects
-#  - MaxRectsBl
-#  - MaxRectsBssf <- Default
-#  - MaxRectsBaf <- Which I see as more aligning optimized
-#  - MaxRectsBlsf
-# from rectpack.maxrects import MaxRectsBaf as algorithm
-
-# https://github.com/secnot/rectpack/blob/master/rectpack/guillotine.py
-# Guillotine algorithms GUILLOTINE-RECT-SPLIT, Selecting one
-# Axis split, and one selection criteria.
-# GuillotineBssfSas
-# GuillotineBssfLas
-# GuillotineBssfSlas
-# GuillotineBssfLlas
-# GuillotineBssfMaxas
-# GuillotineBssfMinas
-# GuillotineBlsfSas
-# GuillotineBlsfLas
-# GuillotineBlsfSlas
-# GuillotineBlsfLlas
-# GuillotineBlsfMaxas
-# GuillotineBlsfMinas
-# GuillotineBafSas
-# GuillotineBafLas
-# GuillotineBafSlas
-# GuillotineBafLlas
-# GuillotineBafMaxas
-# GuillotineBafMinas
+from rectpack import PackingBin, newPacker
 from rectpack.guillotine import GuillotineBssfSlas as algorithm
 
+SIERRA_THICKNESS = 5
+BIN_SIZES = [
+    (1830, 2600 // 1, 1),
+    (1830, 2600 // 1, 1),
+    (1830, 2600 // 1, 1),
+    (1830, 2600 // 2, 2),
+    (1830, 2600 // 2, 2),
+]
+EXCLUDE_COLORS = {
+    'snow', 'lavender', 'lavenderblush', 'lightgrey',
+    'blanchedalmond', 'ghostwhite', 'mintcream', 'ivory', 'white'
+}
 
-def read_file(file, rects={}, mul=1, extra_name='', equivalences={}):
+
+def random_color():
+    while True:
+        color = choice(list(colormap.keys()))
+        if color not in EXCLUDE_COLORS:
+            return color
+
+
+def read_file(file, rects=None, mul=1, extra_name='', equivalences=None):
+    rects = {} if rects is None else rects
+    equivalences = {} if equivalences is None else equivalences
     assert mul > 0
-    with open(file, newline='') as csvfile:
+
+    with open(file, mode='r', encoding='utf-8', errors='replace', newline='') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
         for r, row in enumerate(spamreader):
-            if row[3] in equivalences:
-                material = equivalences[row[3]]
-            else:
-                material = row[3]
+            if len(row) < 16:
+                row = row + [''] * (16 - len(row))
+
+            if len(row) < 16:
+                raise ValueError(f'Fila {r} con columnas insuficientes: {row}')
+
+            material = equivalences.get(row[3], row[3])
             if material not in rects:
                 rects[material] = {}
+
             pre_name = extra_name + '%'
             name = ', '.join([pre_name + a for a in row[5].split(', ')])
             name += f'_{r}'
             msg = f'name: {name}, recs: {rects}, material: {material}'
             assert name not in rects[material], msg
-            rects[material][name] = {}
-            height = float(row[0])
-            rects[material][name]['height'] = height
-            width = float(row[1])
-            rects[material][name]['width'] = width
-            rects[material][name]['mul'] = mul
-            rects[material][name]['cantos'] = ['none', 'none', 'none', 'none']
+
+            rects[material][name] = {
+                'height': float(row[0]),
+                'width': float(row[1]),
+                'mul': mul,
+                'cantos': ['none', 'none', 'none', 'none'],
+            }
+
             for j in range(4):
-                canto = row[12 + j]
-                if canto in equivalences:
-                    rects[material][name]['cantos'][j] = equivalences[canto]
-                else:
-                    rects[material][name]['cantos'][j] = canto
-            # if rects[material][name]['cantos'] != ['', '', '', '']:
-                # assert width >= 120, name
-                # assert height >= 120, name
+                canto = row[12 + j] if 12 + j < len(row) else ''
+                rects[material][name]['cantos'][j] = equivalences.get(canto, canto)
+
     return rects
 
 
 def rect_pack(pieces, material, count=1):
-    ESPESOR_SIERRA = 5
-
-    # BIN_SIZES = [(1830, 2600 // (2**j), (2**j)) for j in range(2)]
-    BIN_SIZES = [(1830, 2600 // 1, 1),
-                 (1830, 2600 // 1, 1),
-                 (1830, 2600 // 1, 1),
-                 (1830, 2600 // 2, 2),
-                 (1830, 2600 // 2, 2),
-                 ]
-
-    # PackingBin = Enum(["BNF", "BFF", "BBF", "Global"])
-    packer = newPacker(bin_algo=PackingBin.BBF, pack_algo=algorithm,
-                       rotation=True)
-
-    if count is None:
-        loops = 1
-    else:
-        loops = count
+    packer = newPacker(bin_algo=PackingBin.BBF, pack_algo=algorithm, rotation=True)
+    loops = 1 if count is None else count
 
     for ll in range(loops):
         for w, h, div in BIN_SIZES:
-            name = ''
-            if div == 1:
-                name = 'whole'
-            elif div == 2:
-                name = 'half'
-            elif div == 4:
-                name = 'quarter'
+            div_name = {1: 'whole', 2: 'half', 4: 'quarter'}.get(div, '')
             if count is None:
-                packer.add_bin(w, h, count=float("inf"),
-                               bid=f'{material}_{name}_{ll}')
+                packer.add_bin(w, h, count=float('inf'), bid=f'{material}_{div_name}_{ll}')
             else:
-                packer.add_bin(w, h, count=1,
-                               bid=f'{material}_{name}_{ll}')
+                packer.add_bin(w, h, count=1, bid=f'{material}_{div_name}_{ll}')
 
-    for piece in pieces.keys():
-        for name in piece.split(', '):
-            for q in range(pieces[piece]['mul']):
-                w = pieces[piece]['width'] + ESPESOR_SIERRA
-                h = pieces[piece]['height'] + ESPESOR_SIERRA
-                if q > 1:
-                    packer.add_rect(w, h, f'{q}_{name}')
-                else:
-                    packer.add_rect(w, h, name)
+    for piece_name, piece_data in pieces.items():
+        for name in piece_name.split(', '):
+            for q in range(piece_data['mul']):
+                w = piece_data['width'] + SIERRA_THICKNESS
+                h = piece_data['height'] + SIERRA_THICKNESS
+                rect_name = f'{q}_{name}' if q > 1 else name
+                packer.add_rect(w, h, rect_name)
 
     packer.pack()
-
     return packer
 
 
@@ -129,204 +98,150 @@ def analyse_packer(packer):
     for rect in packer.rect_list():
         b, _, _, _, _, _ = rect
         bins.append(b)
-    return max(bins) + 1
+    return max(bins) + 1 if bins else 0
 
 
-def init_subplot(bins, offset, size, material):
+def init_subplot(fig, bins, offset, size, material):
     fig.add_subplot(1, bins, offset + 1).title.set_text(material)
     ax = plt.gca()
     ax.plot(*size)
     ax.add_patch(Rectangle((0, 0), *size,
                            edgecolor='blue',
                            facecolor='none',
-                           linewidth=2,
-                           ))
+                           linewidth=2))
     return ax
 
 
-def plot_packers(packers):
-    from PIL.ImageColor import colormap
-    from random import choice
-
-    exclude_colors = ['snow', 'lavender', 'lavenderblush', 'lightgrey',
-                      'blanchedalmond', 'ghostwhite', 'mintcream', 'ivory',
-                      'white']
-
-    while True:
-        rnd_color = choice(list(colormap.keys()))
-        if rnd_color not in exclude_colors:
-            break
-    print(rnd_color)
-
-    # Amount of total bins
-    bins = 0
-    for packer in packers:
-        bins += len(packer)
-
+def plot_packers(fig, packers):
+    bins = sum(len(packer) for packer in packers)
     offset = 0
+    color = random_color()
+
     for packer in packers:
         for abin in packer:
-            ax = init_subplot(bins, offset,
-                              (abin.width, abin.height),
-                              abin.bid)
+            ax = init_subplot(fig, bins, offset, (abin.width, abin.height), abin.bid)
             offset += 1
             for rect in abin:
-                x = rect.x
-                y = rect.y
-                w = rect.width
-                h = rect.height
-                ax.add_patch(Rectangle((x, y), w, h,
-                             edgecolor='white',
-                             facecolor=rnd_color,
-                             linewidth=1))
+                ax.add_patch(Rectangle((rect.x, rect.y), rect.width, rect.height,
+                                       edgecolor='white',
+                                       facecolor=color,
+                                       linewidth=1))
 
 
-def plot_packer(bins, offset, packer):
-    from PIL.ImageColor import colormap
-    from random import choice
-
-    exclude_colors = ['snow', 'lavender', 'lavenderblush', 'lightgrey',
-                      'blanchedalmond', 'ghostwhite', 'mintcream', 'ivory',
-                      'white']
-
+def plot_packer(fig, bins, offset, packer):
     last_b = 0
-    ax = init_subplot(bins, offset,
-                      (packer[last_b].width, packer[last_b].height),
-                      packer[last_b].bid)
-
+    ax = init_subplot(fig, bins, offset, (packer[last_b].width, packer[last_b].height), packer[last_b].bid)
     offset_rt = offset
-
     colors = {}
+
     for rect in packer.rect_list():
         b, x, y, w, h, name = rect
         obj = name.split('%')[0]
         if obj not in colors:
-            while True:
-                rnd_color = choice(list(colormap.keys()))
-                if rnd_color not in exclude_colors:
-                    break
-            colors[obj] = rnd_color
+            colors[obj] = random_color()
+
         if b > last_b:
             offset_rt += 1
-            ax = init_subplot(bins, offset_rt,
-                              (packer[b].width, packer[b].height),
-                              packer[b].bid)
+            ax = init_subplot(fig, bins, offset_rt, (packer[b].width, packer[b].height), packer[b].bid)
             last_b = b
+
         ax.add_patch(Rectangle((x, y), w, h,
                                edgecolor='white',
                                facecolor=colors[obj],
-                               linewidth=1,
-                               ))
+                               linewidth=1))
+
     return offset_rt + 1
 
 
 def write_excel(workbook, material, rects, cm):
     worksheet = workbook.add_worksheet(material)
-
-    # Baumann like
     header = ["Cantidad", "Altura", "Anchura", "ID", "Material", "Rota",
               "canto_1", "canto_2", "canto_3", "canto_4", "Nombre"]
 
-    # write_header
-    row = 0
     for col, item in enumerate(header):
-        worksheet.write(row, col, item)
+        worksheet.write(0, col, item)
 
-    row_offset = 1
-    col_offset = 0
-    for row, rect in enumerate(rects.items()):
-        name = rect[0]
-        data_dict = rect[1]
-        if cm:
-            height = int(data_dict['height']) / 10
-            width = int(data_dict['width']) / 10
-        else:
-            height = data_dict['height']
-            width = data_dict['width']
+    for row, rect in enumerate(rects.items(), start=1):
+        name, data_dict = rect
+        height = int(data_dict['height']) / 10 if cm else data_dict['height']
+        width = int(data_dict['width']) / 10 if cm else data_dict['width']
         mul = data_dict['mul']
         qty = mul * len(name.split(', '))
-        cantos = data_dict['cantos']
+        cantos = list(data_dict['cantos'])
+
         if "Puerta" in name:
             cantos = ['X'] * 4
+
         if cantos[0] == '' and cantos[1] != '':
-            aux = cantos[1]
-            cantos[1] = cantos[0]
-            cantos[0] = aux
+            cantos[1], cantos[0] = cantos[0], cantos[1]
         if cantos[2] == '' and cantos[3] != '':
-            aux = cantos[3]
-            cantos[3] = cantos[2]
-            cantos[2] = aux
-        items_to_write = (qty, height, width, row, material, 1, *cantos, name)
+            cantos[3], cantos[2] = cantos[2], cantos[3]
+
+        items_to_write = (qty, height, width, row - 1, material, 1, *cantos, name)
         for col, item in enumerate(items_to_write):
-            worksheet.write(row + row_offset, col + col_offset, item)
+            worksheet.write(row, col, item)
 
 
-if __name__ == '__main__':
+def main():
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--file", type=str, default=None,
-                        help="Input file.")
-    parser.add_argument("--qty", type=int, default=None,
-                        help="Multiple.")
-    parser.add_argument("--yaml", type=str, default=None,
-                        help="Yaml conf.")
-    parser.add_argument("--excel", action='store_true',
-                        help="Export Excel file.")
-    parser.add_argument("--cm", action='store_true',
-                        help="Use centimeters in exported Excel file.")
-    parser.add_argument("--by_obj", action='store_true',
-                        help="Plot by objects in different colors.")
-
+    parser.add_argument('--file', type=str, default=None, help='Input file.')
+    parser.add_argument('--qty', type=int, default=None, help='Multiple.')
+    parser.add_argument('--yaml', type=str, default=None, help='Yaml conf.')
+    parser.add_argument('--excel', action='store_true', help='Export Excel file.')
+    parser.add_argument('--cm', action='store_true', help='Use centimeters in exported Excel file.')
+    parser.add_argument('--by_obj', action='store_true', help='Plot by objects in different colors.')
     args = parser.parse_args()
 
     if args.yaml is None:
-        assert args.file is not None
+        assert args.file is not None, 'Debe indicar --file o --yaml.'
         qty = args.qty or 1
-
         rects = read_file(args.file, mul=qty)
     else:
-        assert args.file is None
-        assert args.qty is None
+        assert args.file is None, 'No combine --file con --yaml.'
+        assert args.qty is None, 'No combine --qty con --yaml.'
 
-        with open(args.yaml, "r") as stream:
+        with open(args.yaml, 'r', encoding='utf-8') as stream:
             try:
                 yaml_conf = yaml.safe_load(stream)
             except yaml.YAMLError as exc:
-                print(exc)
+                raise RuntimeError(f'YAML inválido: {exc}') from exc
+
+        if yaml_conf is None:
+            raise ValueError('El archivo YAML está vacío.')
+
         rects = {}
         equivalences = {}
-        for conf in yaml_conf.keys():
-            element = yaml_conf[conf]
+        for conf, element in yaml_conf.items():
             if conf == 'equivalences':
                 for k in element:
-                    mat = list(k)[0]
-                    assert mat not in equivalences
+                    mat = next(iter(k))
                     equivalences[mat] = k[mat]
                     print(f'instead {mat} I will use {k[mat]}')
             else:
+                file_name = None
                 qty = 1
                 for k in element:
                     if 'path' in k:
-                        file = k['path']
+                        file_name = k['path']
                     if 'qty' in k:
                         qty = k['qty']
-                rects = read_file(file, rects=rects, mul=qty,
-                                  extra_name=conf, equivalences=equivalences)
+                if file_name is None:
+                    raise ValueError(f'Falta path en la configuración {conf}.')
+                rects = read_file(file_name, rects=rects, mul=qty, extra_name=conf, equivalences=equivalences)
 
     fig = plt.figure()
     packers = []
-    bins = 0
+    total_bins = 0
 
-    # Get total of used bins to set the following subplots
-    # In addition, if asked, one excel is generated for each material
     if args.excel:
         workbook = Workbook('placas.xlsx')
 
-    for i, material in enumerate(rects.keys()):
+    for material in rects.keys():
         packer = rect_pack(rects[material], material)
         packers.append(packer)
-        bins += analyse_packer(packer)
+        total_bins += analyse_packer(packer)
         if args.excel:
             write_excel(workbook, material, rects[material], args.cm)
 
@@ -335,8 +250,13 @@ if __name__ == '__main__':
 
     if args.by_obj:
         offset = 0
-        for i, material in enumerate(rects.keys()):
-            offset = plot_packer(bins, offset, packers[i])
+        for material, packer in zip(rects.keys(), packers):
+            offset = plot_packer(fig, total_bins, offset, packer)
     else:
-        plot_packers(packers)
+        plot_packers(fig, packers)
+
     plt.show()
+
+
+if __name__ == '__main__':
+    main()
